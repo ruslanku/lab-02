982d0aff232bd808034036918299e0ddfae9db7f
soul-of-a-wanderer <114995534+soul-of-a-wanderer@users.noreply.github.com>
2026-01-16 16:55:50 -0700

    lab 2 demo

--- code/app/src/main/java/com/example/listycity/MainActivity.java
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -18,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+
+    Button add_city_button;
+    Button delete_city_button;
+    Button confirm_button;
+    EditText text_input;
+    LinearLayout add_city_prompt;
+    int selectedIndex = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +49,50 @@ public class MainActivity extends AppCompatActivity {
         dataList.addAll(Arrays.asList(cities));
 
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+
         cityList.setAdapter(cityAdapter);
+        cityList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+
+        add_city_button = findViewById(R.id.add_city_button);
+        delete_city_button = findViewById(R.id.delete_city_button);
+        confirm_button = findViewById(R.id.confirm_button);
+        text_input = findViewById(R.id.text_input);
+        add_city_prompt = findViewById(R.id.add_city_prompt);
+
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedIndex = position;
+            cityList.setItemChecked(position, true);
+        });
+
+        add_city_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_city_prompt.setVisibility(View.VISIBLE);
+            }
+        });
+
+        delete_city_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedIndex != -1) {
+                    dataList.remove(selectedIndex);
+                    cityAdapter.notifyDataSetChanged();
+                    selectedIndex = -1;
+                }
+            }
+        });
+
+        confirm_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String new_city = text_input.getText().toString().trim();
+                if (!new_city.isEmpty()) {
+                    dataList.add(new_city);
+                    cityAdapter.notifyDataSetChanged();
+                    add_city_prompt.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
